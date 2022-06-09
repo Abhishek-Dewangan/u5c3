d1d90f99f09d560a1d32f8e2046c824f60c5b834
callmeabhi1997 <callmeabhi1997@gmail.com>
2022-06-10 01:53:21 +0530

    update for heroku deploy

--- index.js
+++ index.js
@@ -1,53 +1,142 @@
 const express = require('express');
-const fs = require('fs');
-const app = express();
-const PORT = process.env.PORT || 8080;
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuid } = require('uuid');
+const { readFile, writeFile } = require('fs');
 
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to My voting system');
+});
+
+// create the user data---->
 app.post('/user/create', (req, res) => {
-  fs.readFile('./db.json', { encoding: 'utf-8' }, (err, data) => {
-    const parsed = JSON.parse(data);
-    let id = uuidv4();
-    req.body.id = id;
-    parsed.users = [...parsed.users, req.body];
-    fs.writeFile(
-      './db.json',
-      JSON.stringify(parsed),
-      { encoding: 'utf-8' },
-      (err) => {
-        if (err) {
-          console.error(err);
-        }
-        res.status(201).send({ status: 'User Created', id });
-      },
-    );
-  });
-});
-
-app.listen(PORT, function (err) {
-  if (err) console.log('Error in server setup');
-  console.log('Server listening on Port', PORT);
-});
-
-app.get('/votes/voters', (req, res) => {
-  fs.readFile('./db.json', 'utf-8', (err, data) => {
-    const parsed = JSON.parse(data);
-    parsed.users = parsed.users.filter((el) => {
-      return el.role === 'voter';
+  if (req.body.id && req.body.name && req.body.age && req.body.role) {
+    readFile('./db.json', 'utf-8', (err, data) => {
+      const parsed = JSON.parse(data);
+      if (req.body.role == 'voter') {
+        parsed.user = [
+          ...parsed.user,
+          { ...req.body, username: req.body.name, password: req.body.name },
+        ];
+      } else {
+        parsed.user = [
+          ...parsed.user,
+          { ...req.body, votes: 0, party: req.body.party },
+        ];
+      }
+      writeFile('./db.json', JSON.stringify(parsed), 'utf-8', () => {
+        res.status(201).send('User created sucessfully');
+      });
     });
-    res.send(parsed);
-  });
+  } else {
+    res.status(402).send('Please provide all the mendetory details');
+  }
 });
+
+// Login user-->
+app.post('/user/login', (req, res) => {
+  if (req.body.username && req.body.password) {
+    readFile('./db.json', 'utf-8', (err, data) => {
+      const parsed = JSON.parse(data);
+      parsed.user = parsed.user.map((el) => {
+        if (
+          el.username == req.body.username &&
+          el.password == req.body.password
+        ) {
+          {
+            req.body.token = uuid();
+          }
+        }
+      });
+      writeFile('./db.json', JSON.stringify(parsed), 'utf-8', () => {
+        res.status(201).send(`Login sucessfully ${req.body.token}`);
+      });
+    });
+  } else {
+    res.status(400).send('please provide username and password');
+  }
+});
+
+// Logout a user -->
+app.post('/user/logout', (req, res) => {
+  if (req.body.username && req.body.password) {
+    readFile('./db.json', 'utf-8', (err, data) => {
+      const parsed = JSON.parse(data);
+      parsed.user = parsed.user.map((el) => {
+        if (
+          el.username == req.body.username &&
+          el.password == req.body.password
+        ) {
+          {
+            req.body.token = null;
+          }
+        }
+      });
+      writeFile('./db.json', JSON.stringify(parsed), 'utf-8', () => {
+        res.status(201).send(`Login sucessfully ${req.body.token}`);
+      });
+    });
+  } else {
+    res.status(400).send('please provide username and password');
+  }
+});
+
+//   List of candidates-->
 app.get('/votes/party/:party', (req, res) => {
   const { party } = req.params;
-  fs.readFile('./db.json', { encoding: 'utf-8' }, (err, data) => {
+  readFile('./db.json', 'utf-8', (err, data) => {
     const parsed = JSON.parse(data);
-    parsed.users = parsed.users.filter((el) => {
-      return el.party === party;
-    });
-    res.send(parsed);
+    parsed.user = parsed.user.filter((elem) => elem.party == party);
+
+    let list = JSON.stringify(parsed);
+    console.log(list);
+    res.status(200).send('candidate list is get' + list);
   });
+});
+
+//   list of voters:-->
+app.get('/votes/voters', (req, res) => {
+  readFile('./db.json', 'utf-8', (err, data) => {
+    const parsed = JSON.parse(data);
+    parsed.user = parsed.user.filter((elem) => elem.voters == voters);
+
+    let list = JSON.stringify(parsed);
+    console.log(list);
+    res.status(200).send('candidate list is get' + list);
+  });
+});
+
+//votes of an user
+app.patch('/votes/vote/:user', (req, res) => {
+  const { user } = req.params;
+
+  readFile('./db.json', 'utf-8', (err, data) => {
+    const parsed = JSON.parse(data);
+    parsed.user = parsed.user.map((elem) =>
+      elem.name == user ? { ...elem, vote: vote++ } : elem,
+    );
+    writeFile('./db.json', JSON.stringify(parsed), 'utf-8', () => {
+      return res.status(201).send(`Vote added sucessfully`);
+    });
+  });
+  res.status(401).send(`Vote not counted`);
+});
+
+app.get('/votes/count/:user', (req, res) => {
+  const { user } = req.params;
+  console.log(user);
+  readFile('./db.json', 'utf-8', (err, data) => {
+    const parsed = JSON.parse(data);
+    parsed.user = parsed.user.filter((elem) => elem.name == user);
+    console.log(parsed.user[0].votes);
+    res.status(200).send(`Total Votes of${user} ${parsed.user[0].votes}`);
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log('Server started on port 8080');
 });
